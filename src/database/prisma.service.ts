825c55d3c84408cae622b99d3efa2a348ee18984
MateusDuraessantos <mateusduraessantos@gmail.com>
2026-03-12 23:52:07 -0300

    CRUD using NestJs

---
+++ src/database/prisma.service.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const url = process.env.DATABASE_URL;
+
+    if (!url) {
+      throw new Error('DATABASE_URL não foi definida');
+    }
+
+    const adapter = new PrismaBetterSqlite3({ url });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
